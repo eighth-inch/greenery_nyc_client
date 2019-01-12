3e1aca0902531617c63be3d45d2f95d32408a527
Daniel Panzer <daniel@cruxcode.nyc>
2019-01-12 11:22:13 -0500

    Initial Commit

---
+++ greenery_nyc_client/greenery_nyc_client.h
@@ -0,0 +1,19 @@
+//
+//  greenery_nyc_client.h
+//  greenery_nyc_client
+//
+//  Created by Daniel Panzer on 1/12/19.
+//  Copyright © 2019 Daniel Panzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for greenery_nyc_client.
+FOUNDATION_EXPORT double greenery_nyc_clientVersionNumber;
+
+//! Project version string for greenery_nyc_client.
+FOUNDATION_EXPORT const unsigned char greenery_nyc_clientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <greenery_nyc_client/PublicHeader.h>
+
+
